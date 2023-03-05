38374a99283be9a3d025a3a57a1bbd566d479e37
Alexey Kuznetsov <leoxander@yandex.ru>
2023-03-05 19:36:07 +0300

    Initial commit

---
+++ src/main/java/edu/alexey/toyslotto/App.java
@@ -0,0 +1,13 @@
+package edu.alexey.toyslotto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
